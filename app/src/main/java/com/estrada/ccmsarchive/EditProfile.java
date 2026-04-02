b84115feaec2a2312f450258fdf1a55639eddd9f
Ann Dominique <estrada.ad@outlook.com>
2026-04-02 16:50:26 +0800

    Added multiple layouts and connections. UI for Change Password done.

---
+++ app/src/main/java/com/estrada/ccmsarchive/EditProfile.java
@@ -0,0 +1,36 @@
+package com.estrada.ccmsarchive;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        TextView headerTitle = findViewById(R.id.header_title);
+        ImageView btnBack = findViewById(R.id.btn_back);
+
+        if (headerTitle != null) {
+            headerTitle.setText(R.string.menu_edit_profile);
+        }
+
+        if (btnBack != null) {
+            btnBack.setOnClickListener(v -> {
+                finish();
+            });
+        }
+    }
+}
