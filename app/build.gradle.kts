f47a17d87e69d45d7a4d098509dbc944bf4f6f41
Jasmine Raviz <jasmineraviz95@gmail.com>
2026-04-16 16:07:45 +0800

    lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,14 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.cardview)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.0")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
 }
+
