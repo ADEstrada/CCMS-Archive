03fa1c40ee2ec3ec39e5021c6c5811bde7d3709c
Ann Dominique C. Estrada <estrada.ad@outlook.com>
2026-05-23 07:30:40 +0800

    Update Instructor.java
    Removed comments in the Instructor class for clarity.

--- app/src/main/java/com/estrada/ccmsarchive/Instructor.java
+++ app/src/main/java/com/estrada/ccmsarchive/Instructor.java
@@ -4,15 +4,13 @@ public class Instructor {
     private String email;
     private String firstName;
     private String lastName;
-    private String year; // Ito ang academic rank sa JSON mo
+    private String year;
 
-    // Getters
     public String getEmail() { return email; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
     public String getRank() { return year; }
 
-    // Setters (Dito mawawala ang error sa JsonHelper)
     public void setFirstName(String firstName) { this.firstName = firstName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
     public void setEmail(String email) { this.email = email; }
