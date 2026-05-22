0944e6896c4bb9d6f2c300dcfd576ccd3f20f830
Ann Dominique C. Estrada <estrada.ad@outlook.com>
2026-05-23 07:38:52 +0800

    Update Student.java

--- app/src/main/java/com/estrada/ccmsarchive/Student.java
+++ app/src/main/java/com/estrada/ccmsarchive/Student.java
@@ -2,12 +2,11 @@ package com.estrada.ccmsarchive;
 
 public class Student {
     private String studentId;
-    private String firstName; // Tugma sa JSON key "firstName"
-    private String lastName;  // Tugma sa JSON key "lastName"
+    private String firstName; 
+    private String lastName;  
     private String program;
-    private String year;      // Tugma sa JSON key "year"
+    private String year;     
 
-    // Getters - Ito ang nag-aalis ng red lines sa Seeder!
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
     public String getStudentId() { return studentId; }
